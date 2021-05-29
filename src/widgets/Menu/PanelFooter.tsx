7b530bbfa17b2ba2e66985fa43d46f483108b213
brianpintar <84141215+brianpintar@users.noreply.github.com>
2021-05-29 23:23:24 +0800

    Update PanelFooter.tsx

--- src/widgets/Menu/PanelFooter.tsx
+++ src/widgets/Menu/PanelFooter.tsx
@@ -22,7 +22,7 @@ const Container = styled.div`
   flex: none;
   padding: 8px 4px;
   background-color: ${({ theme }) => theme.nav.background};
-  border-top: solid 2px rgba(133, 133, 133, 0.1);
+  border-top: 1px solid rgba(18, 53, 88);
 `;
 
 const PriceLink = styled.a`
