6edcf4a40fe83c663c5d94c3e46b082cf225806f
brianpintar <84141215+brianpintar@users.noreply.github.com>
2021-05-30 22:28:33 +0800

    Update index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -343,7 +343,7 @@ var Icon$p = function (props) {
 
 var Icon$q = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 32 32" }, props),
-        React.createElement("image", { width: "55", height: "55", href: "/images/egg/logo.png" })));
+        React.createElement("image", { width: "32", height: "32", href: "/images/egg/logo.png" })));
 };
 
 var Icon$r = function (props) {
@@ -1874,8 +1874,8 @@ var Icon$P = function (props) {
 
 var Logo = function (_a) {
     var isDark = _a.isDark, props = __rest(_a, ["isDark"]);
-    return (React.createElement(Svg, __assign({ viewBox: "0 0 433 55" }, props),
-        React.createElement("image", { width: "433", height: "55", href: isDark ? '/images/egg/LogoTextNewDark.png' : '/images/egg/LogoTextNewWhite.png' })));
+    return (React.createElement(Svg, __assign({ viewBox: "0 0 206 55" }, props),
+        React.createElement("image", { width: "206", height: "55", href: isDark ? '/images/egg/LogoTextNewDark.png' : '/images/egg/LogoTextNewWhite.png' })));
 };
 
 var Icon$Q = function (props) {
